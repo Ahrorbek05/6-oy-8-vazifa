e2aeb64df02a738a7600c112322cbed75064e473
Ahrorbek05 <akramjonovahrorbek@gmail.com>
2024-07-31 13:29:48 +0500

    home work

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import './App.css';
-import SignUp from './components/SignUp';
+import Register from './components/Register';
 import Login from './components/Login';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
+
+  const navigate = useNavigate();
+  function ProtectedRoute({isAuthenticated, children}){
+    if(!isAuthenticated){
+      navigate('/login')
+    }
+    return children;
+  }
+  
+
   return (
     <Routes>
-      <Route path='/' element={<SignUp></SignUp>} />
+      <Route path='/register' element={<Register></Register>} />
       <Route path='/login' element={<Login></Login>} />
+      <Route index element = {<ProtectedRoute isAuthenticated={false}><Home></Home></ProtectedRoute>}></Route>
     </Routes>
   );
 }
